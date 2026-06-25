65212286d3dd4c67051aad49e1c6957af9ba6af3
EunjiNim <krelena@naver.com>
2026-06-25 10:36:56 +0900

    config 설정 변경

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,4 +12,9 @@ export default defineConfig({
     reuseExistingServer: true,
   },
   retries: 1, // flaky 판정을 위한 자동 재시도
+  
+  expect: {
+    // 픽셀이 최대 5%까지 달라도 통과(Pass)시켜줌 (숫자는 0.05 ~ 0.1 사이로 조절 가능)
+    toHaveScreenshot: { maxDiffPixelRatio: 0.05 } 
+  },
 });
